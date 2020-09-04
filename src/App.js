5656180a1ffe630f89c2102c0f66225c96361dae
HardyValen <hardyvalen@gmail.com>
2020-09-04 18:17:44 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './css/main.scss';
+
+function App() {
+  return (
+    <div>
+      <span className="font title poppins">Society of Renewable Energy</span>
+      <h1 className="color primary">Society of Renewable Energy</h1>
+      <h2>Society of Renewable Energy</h2>
+      <h3>Society of Renewable Energy Institut Teknologi Bandung</h3>
+      <big>Society of Renewable Energy Institut Teknologi Bandung</big>
+      <p>Society of Renewable Energy Institut Teknologi Bandung</p>
+      <small>Society of Renewable Energy Institut Teknologi Bandung</small>
+    </div>
+  );
+}
+
+export default App;
